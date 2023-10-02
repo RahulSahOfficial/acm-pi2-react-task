11abc2d323914c01a979a9f56f3ab166bf4674d7
Developer Sah <62435110+RahulSahOfficial@users.noreply.github.com>
2023-10-02 16:44:39 +0530

    Create MissingNo.cpp

---
+++ CProgram/MissingNo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n=5,sum=0,expSum,missing;
+    int arr[n]={1,3,4,5};
+    for(int i=0;i<n-1;i++)
+        sum+=arr[i];
+    expSum=(n*(n+1))/2;
+    missing=expSum-sum;
+    cout<<"Missing : "<<missing;
+    return 0;
+}
